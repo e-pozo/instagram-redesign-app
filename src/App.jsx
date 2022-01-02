5b230b4c178d882acde1fb40c57992b4dc6dd5c3
epozo <eduardo.pozo.14@sansano.usm.cl>
2022-01-01 23:16:59 -0300

    Configuracion base

---
+++ src/App.jsx
@@ -0,0 +1,3 @@
+import React from "react";
+
+export const App = () => <h1>Hola este es un texto h1</h1>;
---
+++ webpack.config.dev.js
@@ -0,0 +1,32 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  devtool: "source-map",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "app.bundle.js",
+    publicPath: "/",
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+};
