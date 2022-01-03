c92c0a22f9ff06d7de27526cd7fa2c8775b90598
epozo <eduardo.pozo.14@sansano.usm.cl>
2022-01-03 00:30:09 -0300

    visualizacion de postInfo hecha

--- src/components/PostInfo/index.jsx
+++ src/components/PostInfo/index.jsx
@@ -1,5 +1,17 @@
 import React from "react";
-import { Flex } from "./styles";
+import { Flex, Number, StatsContainer, Subject } from "./styles";
+const Stat = ({ amount, subject }) => (
+  <StatsContainer>
+    <Number>{amount}</Number>
+    <Subject>{subject}</Subject>
+  </StatsContainer>
+);
 export const PostInfo = () => {
-  return <Flex></Flex>;
+  return (
+    <Flex>
+      <Stat subject="Posts" amount="524" />
+      <Stat subject="Followers" amount="124K" />
+      <Stat subject="Following" amount="248" />
+    </Flex>
+  );
 };
--- src/components/PostInfo/styles.js
+++ src/components/PostInfo/styles.js
@@ -1,8 +1,24 @@
 import styled from "styled-components";
+import { GREY } from "../../styles/ColorsShadowsAnimations";
 
 export const Flex = styled.div`
   width: 100%;
-  height: 14vh;
+  height: 6vh;
   display: flex;
   justify-content: space-evenly;
+`;
+
+export const Number = styled.h2`
+  font-size: 1.2rem;
+`;
+
+export const Subject = styled.h2`
+  font-size: 1.2rem;
+  color: ${GREY};
+`;
+
+export const StatsContainer = styled.div`
+  height: 100%;
+  width: 50px;
+  text-align: center;
 `;
