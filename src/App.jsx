10880ac6a811cc8b82e37b269677e1d940a5775a
epozo <eduardo.pozo.14@sansano.usm.cl>
2022-01-04 16:51:24 -0300

    ButtonPicture se centra bien ahora

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import React from "react";
 import { GlobalStyle } from "./styles/GlobalStyles";
-import { Profile } from "./pages/Profile";
+// import { Profile } from "./pages/Profile";
 import { Home } from "./pages/Home";
+// import { ButtonPicture } from "./components/ButtonPicture";
+// import dogPicture from "@images/photo-1518155317743-a8ff43ea6a5f.jpeg";
+
 export const App = () => {
   return (
     <>
