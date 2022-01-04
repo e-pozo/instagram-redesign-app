a6d4cdc3d7ebee63f6b841fab1cc0aff90aecf8e
epozo <eduardo.pozo.14@sansano.usm.cl>
2022-01-04 16:38:02 -0300

    visualizacion de userlist hecha

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { HomeHeader } from "../../components/HomeHeader";
 import { PhotoCard } from "../../components/PhotoCard";
+import { UserList } from "../../components/UserList";
 import { SPALayout } from "../SPAScroll";
 export const Home = () => {
   return (
     <SPALayout>
       <HomeHeader />
+      <UserList />
+      <PhotoCard />
+      <PhotoCard />
+      <PhotoCard />
       <PhotoCard />
     </SPALayout>
   );
